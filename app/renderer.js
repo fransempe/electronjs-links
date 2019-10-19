0078d502d9188f05f44cf986922f8cc560300703
fransempe <fran.sempe@gmail.com>
2019-10-19 11:47:21 -0300

    Last commit

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,3 @@
-import { parse } from "querystring"
-
 // DOM Elements
 const linksSection = document.querySelector('.links')
 const errorMessage = document.querySelector('.error-message')
@@ -11,23 +9,85 @@ const clearStorageButton = document.querySelector('.clear-storage')
 
 // DOM APIs
 const parser = new DOMParser();
+const { shell } = require('electron')
 const parserResponse = text => {
    return parser.parseFromString(text,'text/html');
 }
 
+
+const findTitle = (nodes) => {
+    return nodes.querySelector('title').innerText;   
+}
+
+const storeLink = (title , url) => {
+    localStorage.setItem(url, JSON.stringify({title, url}))
+}
+
+const getLinks = () => {
+   return  Object.keys(localStorage)
+    .map(key => JSON.parse(localStorage.getItem(key)))
+} 
+
+const createLinkElement = link => {
+    return `
+        <div>
+            <h3>${link.title}</h3>
+            <p>
+                <a href="${link.url}">${link.url}</a>
+            </p>
+        </div>
+    `;
+}
+
+const renderLinks = () => {
+    const linksElements = getLinks().map(createLinkElement).join('');
+    linksSection.innerHTML = linksElements;
+}
+
+const clearForm = () => {
+    newLinkUrl.value = null;
+}
+
+const handleError = (error, url) => {
+    errorMessage.innerHTML =`
+        There was an issue adding "${url}" : ${error.message}
+    `.trim();
+    setTimeout(() => {
+       errorMessage.innerHTML = null; 
+    }, 5000);
+}
+
 // Events
+renderLinks();
 newLinkUrl.addEventListener('keyup', () => {
     newLinkButton.disabled = !newLinkUrl.validity.valid;
 });
 
 
-
 newLinkForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const url = newLinkUrl.value;
-    const response = await fetch(url);
-    const text = await response.text();
+   try {
+        const response = await fetch(url);
+        const text = await response.text();
+        const html = parserResponse(text);
+        const title = findTitle(html);
+        storeLink(title, url);
+        clearForm();
+        renderLinks();
+   } catch (e) {
+        handleError(e, url);
+   }
+});
 
-    const html = parserResponse(text)
-    console.log(html);
+clearStorageButton.addEventListener('click', () => {
+    localStorage.clear();
+    linksSection.innerHTML = '';
+});
+
+linksSection.addEventListener('click', (e) => {
+    if(e.target.href){
+       e.preventDefault();
+       shell.openExternal(e.target.href);
+    }
 })
